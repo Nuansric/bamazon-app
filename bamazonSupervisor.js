d4fb49b723e5fe9ebb53e641aada4cd080134334
Chanita Nuansri <nuansric@Chanitas-MacBook-Pro.local>
2017-02-04 16:50:56 -0600

    bamazonSupervisor.js commented

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,54 +1,48 @@
+//Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var importedPassword = require("./key.js");
 var table =  require("console.table");
+
+//gloabal variable for timer
 var delayTimer;
 
-
+//mySQL
 var connection = mysql.createConnection({
 	host: "localhost",
-	port: 3306,
+	//port: 3306,
 	user: "root",
 	password: importedPassword.mySQLKeys.password,
 	database: "Bamazon_db"
 });
 
-
+//test connection
 connection.connect(function(err) {
 	if (err) throw err;
-	// console.log("connected as id " + connection.threadId);
-	// console.log("\n");
+
 
 });
 
+//
 function getProfit(){
 	 
-
 	connection.query("SELECT * FROM departments", function(err, res){
 		
+		//storing response in a local variable
 		var departmentLookup = res;
 
-		
-		
-		//console.log(departmentLookup);
-
-		 for (var i=0; i<departmentLookup.length; i++){
-
-		departmentLookup[i].total_profit = parseFloat(departmentLookup[i].total_sales) - parseFloat(departmentLookup[i].over_head_costs);
-	
-		}
-	
-		console.table(departmentLookup);
+			for (var i=0; i<departmentLookup.length; i++){
+				//calculate the total profit and adding it into each object in the array as a new property
+				departmentLookup[i].total_profit = parseFloat(departmentLookup[i].total_sales) - parseFloat(departmentLookup[i].over_head_costs);	
+			}
+			//display newly added table
+			console.table(departmentLookup);
 		});
-
-				delayTimer = setTimeout(supervisorOptions, 1*1000);
-
-
+			//call supervisor questions after 1 second
+			delayTimer = setTimeout(supervisorOptions, 1*1000);
 }
 
-
-
-
+//adding new department questions
 function newDept(){
 
 	inquirer.prompt([{
@@ -85,20 +79,21 @@ function newDept(){
 		}
 	}]).then(function(answer){
 
-connection.query("INSERT INTO departments SET ?", [{department_name: answer.departmentName, over_head_costs: answer.overHead, total_sales: answer.saleTotal}], function(err, res){
+			//insert new row into the table where data is from user input
+			connection.query("INSERT INTO departments SET ?", [{department_name: answer.departmentName, over_head_costs: answer.overHead, total_sales: answer.saleTotal}], function(err, res){
 
-			console.log("New Department added!!");
-});
+				console.log("New Department added!!");
+			});
 
+		//call supervisor questions after 1 second
 		delayTimer = setTimeout(supervisorOptions, 1*1000);
 	});
 			
-
-
 }
+
+//question set for supervisor to pick
 function supervisorOptions(){
-
-
+	//clear Timer
 	clearTimeout(delayTimer);
 
 	inquirer.prompt([{
